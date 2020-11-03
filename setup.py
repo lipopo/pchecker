0e10e9c0535942e13122f78017bd2cc53efb5c06
lipo <lipo8081@gmail.com>
2020-11-03 19:43:08 +0800

    变更版本

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "rb") as f:
 
 setup(
     name="ParameterChecker",
-    version="0.0.3",
+    version="0.0.4",
     author="lipo",
     author_email="lipo8081@gmail.com",
     description="Parameter manager system",
